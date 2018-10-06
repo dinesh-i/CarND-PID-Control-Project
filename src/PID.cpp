6dfd20b8618dcc6df7c493720d30acf5326ca037
Dinesh Kumar Ilangovan <dinesh.ilangovan@tavant.com>
2018-10-06 17:18:30 +0530

    Basic implementation

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,25 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+	this -> Kp = Kp;
+	this -> Ki = Ki;
+	this -> Kd = Kd;
+
+	p_error = i_error = d_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+	p_error = cte;
+	d_error = cte - d_error;
+	i_error += cte;
 }
 
 double PID::TotalError() {
+	double result = - Kp * p_error - Kd * d_error - Ki * i_error;
+	if( result < -1.0 )
+		return -1.0;
+	if( result > 1.0 )
+		return 1.0;
+	return result;
 }
 
